0737a6351789f2381d504ba1df124239d6e413a1
alvarpao <paola.alvarez@hp.com>
2016-04-26 17:02:21 -0700

    - Initial commit for "Build it Bigger" project.
    - Added test device IDs to the adRequest in the MainActivityFragment to
    get test ads working on all my test devices

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,39 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("81CE8DD28F612B82D5C6450ADF060F75")
+                .addTestDevice("72956278183EB410BB9365FE57C5FD8B")
+                .addTestDevice("86E26955642178AD4BBA445E9F8E5C68")
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
+    }
+}
