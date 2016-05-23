16a26ec8aa977e0173406fada674ac9e7deb69a4
alvarpao <paola.alvarez@hp.com>
2016-05-23 15:11:48 -0700

    - Added two flavors to the app: free and paid. The paid one doesn't show ads whereas the free one does
    - Added a dependency to the google play ads library to the freeCompile configuration since the free version
    of the app is the only one that uses ads now
    
    - Added a specific version of AndroidManifest file to the free version of the app that contains the appropriate
    permissions for Google Mobile ads, some other meta-data specific to google play services and the declaration of
    the AdActivity
    
    - Added different layouts for the main fragment. For the free version of the app the layout contains an AdView to
    display the add. For the paid version of the app added instead of the AdView a TextView that just displays, for now,
    "No ADS here!!"
    
    - Added specific strings for each version of the app to be display depending on which one is ran and installed on
    the device

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
+
+/**
+ * Created by alvarpao on 5/19/2016.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return root;
+    }
+}
